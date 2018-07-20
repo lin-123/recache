f52f307db151a1ee38603b5a3c59b648002ff356
JDJR\houlinjiang <houlinjiang@jd.com>
2018-07-20 18:42:02 +0800

    fix name

--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ const Recache = require('./recache')
 const {version} = require('../package.json')
 
 program.arguments('<dir>')
-       .version(version)
-       .option('-t, --types <type>', 'recache file types, default is <script,style,link>')
-       .option('-q, --quiet', 'recache will not print anything')
-       .option('-r, --restore', 'remove t=timestemp from files')
-       .action(function(dir) {
-        new Recache(dir, program)
-       })
-       .parse(process.argv);
+  .name('recache')
+  .version(version)
+  .option('-t, --types <type>', 'recache file types, default is <script,style,link>')
+  .option('-q, --quiet', 'recache will not print anything')
+  .option('-r, --restore', 'remove t=timestemp from files')
+  .action(function(dir) {
+    new Recache(dir, program)
+  })
+  .parse(process.argv);
